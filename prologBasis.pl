5629f8b96059709bf7f4034f7d09520bfa84c782
Dhruva369 <118178417+Dhruva369@users.noreply.github.com>
2024-02-07 22:46:32 +0530

    Basics of Prolog

---
+++ prologBasis.pl
@@ -0,0 +1,10 @@
+owns(harry, color(green)).   %harry owns green color
+owns(louis, color(blue)).
+owns(liam, color(yellow)).
+owns(zayn, color(red)).
+owns(niall, color(orange)).
+cold(color(green)).    
+cold(color(blue)).
+warm(color(red)).
+warm(color(orange)).
+warm(color(yellow)).        %yellow is warm color
